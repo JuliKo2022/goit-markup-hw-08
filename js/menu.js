cd3366094fea32bdec92491f0d9018f746c2ee98
Julia Kolesnyk <kolesnykjul@gmail.com>
2022-08-24 17:24:54 +0300

    Changes

--- js/menu.js
+++ js/menu.js
@@ -1,16 +1,16 @@
 (() => {
   const refs = {
-    openMenuBtn: document.querySelector('.menu-open-btn'),
-    closeMenuBtn: document.querySelector('.menu-close-btn'),
-    menu: document.querySelector('.mob-menu'),
-    body: document.querySelector('body'),
+    openMenuBtn: document.querySelector("[data-menu-open]"),
+    closeMenuBtn: document.querySelector("[data-menu-close]"),
+    menu: document.querySelector("[data-menu]"),
+    body: document.querySelector("body"),
   };
 
-  refs.openMenuBtn.addEventListener('click', toggleMenu);
-  refs.closeMenuBtn.addEventListener('click', toggleMenu);
+  refs.openMenuBtn.addEventListener("click", toggleMenu);
+  refs.closeMenuBtn.addEventListener("click", toggleMenu);
 
   function toggleMenu() {
-    refs.menu.classList.toggle('is-hidden');
-    refs.body.classList.toggle('no-scroll');
+    refs.menu.classList.toggle("is-hidden");
+    refs.body.classList.toggle("no-scroll");
   }
 })();
